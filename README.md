34a25007eb9834d8217a3e3045a684a3408a8e9d
Zaheer <112798052+Zaheer-10@users.noreply.github.com>
2023-08-02 12:58:05 +0530

    connected dagshub uri

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ML-Flow-Demo
-MLFlow and Dagshub
+
+# MLFlow and Dagshub
+
+```
+MLFLOW_TRACKING_URI=https://dagshub.com/Zaheer-10/ML-Flow-Demo.mlflow \
+MLFLOW_TRACKING_USERNAME=Zaheer-10 \
+MLFLOW_TRACKING_PASSWORD=c73afb9d______________71f72dce3e66f85a \
+python script.py
+
+```
--- app.py
+++ app.py
@@ -75,6 +75,9 @@ if __name__ == "__main__":
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
 
+        ## for remote dagshub
+        remote_server_uri = "https://dagshub.com/Zaheer-10/ML-Flow-Demo.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
